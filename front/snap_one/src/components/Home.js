4d2bcfcab6b4d4ca274b1891ea4c49327deb728a
İrem Bengü Bal <irembengubal@gmail.com>
2024-12-15 22:52:19 +0300

    design
    son düzeltmeler

--- front/snap_one/src/components/Home.js
+++ front/snap_one/src/components/Home.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
-import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
 function Home() {
   const [userInfo, setUserInfo] = useState(null);
-  const [photos, setPhotos] = useState([]); // Fotoğrafları tutacak state
-  const [selectedPhoto, setSelectedPhoto] = useState(null); // Seçilen fotoğraf
-  const [rating, setRating] = useState(""); // Puan
+  const [photos, setPhotos] = useState([]);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [rating, setRating] = useState(0); // Varsayılan 0 olarak başlatıldı
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,20 +15,17 @@ function Home() {
     const username = sessionStorage.getItem("username");
     const userId = sessionStorage.getItem("userId");
 
-    // Eğer gerekli bilgiler yoksa, login sayfasına yönlendir
     if (!apiKey || !username || !userId) {
       navigate("/login");
       return;
     }
 
-    // Kullanıcı bilgilerini state'e kaydet
     setUserInfo({
       userId,
       username,
-      apiKey, // API key de state'e eklenmeli
+      apiKey,
     });
 
-    // Kullanıcıya ait fotoğrafları çek
     const fetchPhotos = async () => {
       try {
         const response = await fetch(
@@ -37,7 +33,7 @@ function Home() {
         );
         const data = await response.json();
         if (response.ok) {
-          setPhotos(data); // Fotoğraf ID'lerini kaydet
+          setPhotos(data);
         } else {
           console.error("Error fetching photos:", data.error);
         }
@@ -46,17 +42,20 @@ function Home() {
       }
     };
 
-    fetchPhotos(); // fetchPhotos'u çağırıyoruz
-  }, [navigate]); // 'navigate' dependency'si eklendi
+    fetchPhotos();
+  }, [navigate]);
 
-  // Fotoğraf seçildiğinde çalışacak fonksiyon
   const handleImageClick = (photo) => {
     setSelectedPhoto({ id: photo });
   };
 
+  const handleStarClick = (index) => {
+    setRating(index + 1); // Yıldızlara tıklayınca rating'i günceller
+  };
+
   const handleSubmitRating = async () => {
     if (selectedPhoto && rating) {
-      const { userId, apiKey, username } = userInfo || {}; // userInfo'dan çekiliyor
+      const { userId, apiKey, username } = userInfo || {};
 
       try {
         const response = await fetch(
@@ -68,8 +67,8 @@ function Home() {
 
         if (response.ok) {
           alert("Rating submitted successfully!");
-          setRating(""); // Puanlama sonrası input temizlenir
-          setSelectedPhoto(null); // Modal kapatılır
+          setRating(0);
+          setSelectedPhoto(null);
         } else {
           const errorData = await response.json();
           alert(`Failed to submit rating: ${errorData.message}`);
@@ -83,95 +82,66 @@ function Home() {
     }
   };
 
-  // Fotoğraf silme işlemi
-  const handleDeletePhoto = async (photoId) => {
-    const { apiKey, username } = userInfo || {};
-
-    try {
-      const response = await fetch(
-        `/api/photos/${photoId}?api=${apiKey}&uname=${username}`,
-        {
-          method: "DELETE",
-        }
-      );
-
-      if (response.ok) {
-        alert("Photo deleted successfully!");
-        // Fotoğrafı fotoğraflar listesinden kaldır
-        setPhotos(photos.filter((photo) => photo !== photoId));
-      } else {
-        const errorData = await response.json();
-        alert(`Failed to delete photo: ${errorData.message}`);
-      }
-    } catch (error) {
-      console.error("Error while deleting photo:", error);
-      alert("An error occurred while deleting the photo.");
-    }
-  };
-
-  const handleRatingChange = (e) => {
-    setRating(Number(e.target.value)); // Kullanıcının girdiği puanı sayıya çeviriyoruz
-  };
-
   return (
     <>
-      <div className="pinterest-feed">
-        {photos.map((photoId) => (
-          <div key={photoId} className="pinterest-item">
-            <img
-              key={photoId}
-              src={`/api/photos/${photoId}?api=${userInfo?.apiKey}&uname=${userInfo?.username}`}
-              alt={`Photo ${photoId}`}
-              onClick={() => handleImageClick(photoId)}
-            />
-            <Button variant="danger" onClick={() => handleDeletePhoto(photoId)}>
-              Delete Photo
-            </Button>
-          </div>
-        ))}
+      <div className="content">
+        <div className="pinterest-feed">
+          {photos.map((photoId) => (
+            <div key={photoId} className="pinterest-item">
+              <img
+                src={`/api/photos/${photoId}?api=${userInfo?.apiKey}&uname=${userInfo?.username}`}
+                alt={`Photo ${photoId}`}
+                onClick={() => handleImageClick(photoId)}
+              />
+            </div>
+          ))}
+        </div>
       </div>
 
-      {/* Modal: Fotoğrafın büyük hali ve puanlama kısmı */}
       {selectedPhoto && (
-        <Modal
-          show={true}
-          onHide={() => setSelectedPhoto(null)} // Modal'ı kapat
-          backdrop="static"
-          centered // Modal'ı kapat
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Rate this photo</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="modal-image-container">
-              <img
-                src={`/api/photos/${selectedPhoto.id}?api=${userInfo?.apiKey}&uname=${userInfo?.username}`}
-                alt="Selected Photo"
-                className="modal-image"
-                style={{ width: "30%", height: "auto" }}
-              />
+        <div className="modal-backdrop">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                className="close-button"
+                onClick={() => setSelectedPhoto(null)}
+              >
+                ✕
+              </button>
             </div>
-            <div className="rating-container">
-              <h4>Rate this photo:</h4>
-              <input
-                type="number"
-                min="1"
-                max="5"
-                value={rating}
-                onChange={handleRatingChange}
-                placeholder="Rate (1-5)"
-              />
+            <div className="modal-body">
+              <div className="modal-image-container">
+                <img
+                  src={`/api/photos/${selectedPhoto.id}?api=${userInfo?.apiKey}&uname=${userInfo?.username}`}
+                  alt="Selected Photo"
+                  className="modal-image"
+                />
+              </div>
+              <div className="rating-container">
+                <h4>Rate this photo:</h4>
+                <div className="star-rating">
+                  {[...Array(5)].map((_, index) => (
+                    <span
+                      key={index}
+                      className={`star ${index < rating ? "selected" : ""}`}
+                      onClick={() => handleStarClick(index)}
+                    >
+                      ★
+                    </span>
+                  ))}
+                </div>
+              </div>
             </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => setSelectedPhoto(null)}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleSubmitRating}>
-              Submit Rating
-            </Button>
-          </Modal.Footer>
-        </Modal>
+            <div className="modal-footer">
+              <button
+                className="submit-button"
+                onClick={() => handleSubmitRating()}
+              >
+                Submit Rating
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
--- front/snap_one/src/components/Navbar.js
+++ front/snap_one/src/components/Navbar.js
@@ -2,6 +2,12 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css"; // CSS dosyasını import ediyoruz
 import cavi from "./images/cavi.jpeg";
+import login from "./images/login.jpg";
+import register from "./images/register.jpg";
+import home from "./images/home.jpg";
+import logout from "./images/logout.jpg";
+import profile from "./images/profile.jpg";
+import list from "./images/list.png";
 
 function Navbar() {
   const navigate = useNavigate();
@@ -22,41 +28,41 @@ function Navbar() {
       <ul className="navbar-list">
         <li className="navbar-item">
           <Link to="/" className="navbar-link">
-            <img src={cavi} alt="Home" className="navbar-icon" />
+            <img src={home} alt="Home" className="navbar-icon" />
             <span>Home</span>
           </Link>
         </li>
         <li className="navbar-item">
           <Link to="/login" className="navbar-link">
-            <img src={cavi} alt="Login" className="navbar-icon" />
+            <img src={login} alt="Login" className="navbar-icon" />
             <span>Giriş</span>
           </Link>
         </li>
         <li className="navbar-item">
           <Link to="/register" className="navbar-link">
-            <img src={cavi} alt="Register" className="navbar-icon" />
+            <img src={register} alt="Register" className="navbar-icon" />
             <span>Kayıt Ol!</span>
           </Link>
         </li>
         <li className="navbar-item">
           <Link to="/profile" className="navbar-link">
-            <img src={cavi} alt="Profile" className="navbar-icon" />
+            <img src={profile} alt="Profile" className="navbar-icon" />
             <span>Profil</span>
           </Link>
         </li>
         <li className="navbar-item">
           <Link to="/list" className="navbar-link">
-            <img src={cavi} alt="Profile" className="navbar-icon" />
+            <img src={list} alt="Profile" className="navbar-icon" />
             <span>Listeler</span>
           </Link>
         </li>
-        <li className="navbar-item">
-          <button onClick={handleLogout}>
-            <img src="/images/cavi.jpeg" alt="Logout" className="navbar-icon" />
-            <span>çıkış</span>
-          </button>
-        </li>
       </ul>
+      <div className="logout-btn-container">
+        <button className="logout-btn" onClick={handleLogout}>
+          <img src={logout} alt="Logout" className="navbar-icon" />
+          <span>Çıkış</span>
+        </button>
+      </div>
     </nav>
   );
 }
